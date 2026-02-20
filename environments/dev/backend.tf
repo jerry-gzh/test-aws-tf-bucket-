08a14b0745c9a074c819f13933cfd99cadcf237e
Gerardo Andres Guzman Hernandez <guhg1559082@cmv.mx>
2026-02-20 15:55:30 -0600

    Setup terraform backend and dev environment

---
+++ environments/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "jerry-infra-tfstates-dev"
+    key            = "test-aws-tf-bucket/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
